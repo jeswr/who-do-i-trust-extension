c9e33f18e6a3b00d814b2c274d5eac72ef17a893
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-05-18 21:32:00 +0100

    WIP: setup icon management

--- background.ts
+++ background.ts
@@ -1,9 +1,15 @@
 console.log('Hello from the background script!')
 
-chrome.action.enable()
+function setRating(good = false, tabId: number | undefined = undefined) {
+    chrome.action.setBadgeText({ text: good ? '👍' : '👎', tabId });
+    chrome.action.setBadgeBackgroundColor({ color: good ? 'green' : 'red', tabId });
+}
 
-// background.js
-chrome.action.onClicked.addListener((tab) => {
-    chrome.action.enable()
+chrome.tabs.query({}, tabs => tabs.forEach(tab => setRating(typeof tab.url === 'string' && new URL(tab.url).origin === 'http://example.org/', tab.id)));
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log(tabId, changeInfo.url, tab, chrome.action)
+    const urlText = changeInfo.url ?? 'no url';
+    console.log('the url text is', urlText)
+    setRating(typeof tab.url === 'string' && new URL(tab.url).origin === 'http://example.org/', tabId);
 });
-  
